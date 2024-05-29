ddda12724c11455f3580170671de7046860e0ee2
ricardo-nikolay2 <ricardo.nikolay@gmail.com>
2024-05-29 18:49:34 +0700

    menambahkan paragraph pada file index.php

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    Autem, saepe debitis perferendis eaque quo voluptates repellat,
+     expedita quia, voluptas quasi dolorem sapiente eius error sequi.
+      Nulla sapiente harum quae esse?</P>
 </body>
 </html>
